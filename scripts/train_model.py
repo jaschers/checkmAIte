ce8e4660eb8d85295a346ba16f29b6d1cda629d1
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-08-23 18:54:46 +0200

    read multiple data files

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,14 +1,26 @@
 import numpy as np
 from utilities import *
-from tensorflow.keras import models
-from tensorflow.keras.layers import Conv2D, GlobalMaxPooling2D, Dense, Flatten, Input
-import tensorflow.keras.utils as utils
-from tensorflow.keras.callbacks import CSVLogger
+from keras import models
+from keras.layers import Conv2D, GlobalMaxPooling2D, Dense, Flatten, Input
+import keras.utils as utils
+from keras.callbacks import CSVLogger
 import pandas as pd
 import os
+import time
 
 # load data
-table = pd.read_hdf("data/data.h5", key = "table")
+num_runs = 6
+table = pd.DataFrame()
+for run in range(num_runs):
+    print(f"Loading data run {run}...")
+    start = time.time()
+    table_run = pd.read_hdf(f"data/data{run}.h5", key = "table")
+    middle = time.time()
+    print(f"Data run {run} loaded in {np.round(middle-start)} sec...")
+    frame = [table, table_run]
+    table = pd.concat(frame)
+    end = time.time()
+    print(f"Tables combined in {np.round(end-middle)} sec...")
 table = table.dropna().reset_index()
 
 X = table["boards (int)"].values.tolist()
@@ -27,6 +39,7 @@ Y_train, Y_val, Y_test = np.split(Y, [-int(len(X) / 5), -int(len(Y) / 10)])
 print(np.shape(X_train), np.shape(X_val), np.shape(X_test))
 print(np.shape(Y_train), np.shape(Y_val), np.shape(Y_test))
 
+# define model
 model_input = Input(shape = X_shape[1:])
 model = Conv2D(16, kernel_size = (3, 3), activation = "relu", padding = "same")(model_input)
 model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64], increase_dim = True)
@@ -39,15 +52,17 @@ model = Dense(64, activation = "relu")(model)
 model = Dense(1, name = "score")(model)
 
 # # define model
-# model_input = layers.Input(shape = X_shape[1:])
-# model = layers.Conv2D(16, (3, 3), activation = "relu", padding = "same")(model_input)
-# model = layers.Conv2D(32, (3, 3), activation = "relu", padding = "same")(model)
-# model = layers.Conv2D(64, (3, 3), activation = "relu", padding = "same")(model)
-# model = layers.GlobalMaxPooling2D()(model)
-# model = layers.Flatten()(model)
-# model = layers.Dense(64, activation = "relu")(model)
-# # model = layers.Dense(16, activation = "relu")(model)
-# model = layers.Dense(1, name = "score")(model)
+# model_input = Input(shape = X_shape[1:])
+# model = Conv2D(16, (3, 3), activation = "relu", padding = "same")(model_input)
+# model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model)
+# model = Conv2D(64, (3, 3), activation = "relu", padding = "same")(model)
+# model = Conv2D(128, (3, 3), activation = "relu", padding = "same")(model)
+# model = GlobalMaxPooling2D()(model)
+# model = Flatten()(model)
+# model = Dense(128, activation = "relu")(model)
+# model = Dense(64, activation = "relu")(model)
+# # model = Dense(16, activation = "relu")(model)
+# model = Dense(1, name = "score")(model)
 
 model = models.Model(inputs = model_input, outputs = model)
 model.summary()
@@ -60,6 +75,6 @@ model.compile(optimizer = "adam",
               loss="mse")
 
 os.makedirs("history/", exist_ok = True)
-model.fit(X_train, Y_train, epochs = 10, batch_size = 64, validation_data=(X_val, Y_val), callbacks = [CSVLogger("history/history.csv")])
+model.fit(X_train, Y_train, epochs = 20, batch_size = 64, validation_data=(X_val, Y_val), callbacks = [CSVLogger("history/history.csv")])
 
 model.save("model/model.h5")
