e53e4736d237e0a706052962fff1e2b30cf85875
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-08-30 15:30:29 +0200

    add saving of score distribution

--- scripts/evaluate_data.py
+++ scripts/evaluate_data.py
@@ -1,18 +1,21 @@
 import numpy as np
-from utilities import *
 import pandas as pd
 import time
-from chessboard import display
+# from chessboard import display
 import matplotlib.pyplot as plt
+import os
+import sys
+np.set_printoptions(threshold=sys.maxsize)
 
 # load data
-num_runs = 1
+num_runs = 35
 table = pd.DataFrame()
 for run in range(num_runs):
-    run = 2 #2
+    # run = 2 #2
     print(f"Loading data run {run}...")
     start = time.time()
     table_run = pd.read_hdf(f"data/data{run}.h5", key = "table")
+    print(f"Number of boards in run {run}:", len(table_run))
     middle = time.time()
     print(f"Data run {run} loaded in {np.round(middle-start)} sec...")
     frame = [table, table_run]
@@ -20,6 +23,7 @@ for run in range(num_runs):
     end = time.time()
     print(f"Tables combined in {np.round(end-middle)} sec...")
 
+table = table.reset_index(drop = True)
 print(table)
 # duplicates = table.duplicated(subset=["board (int)"])
 # print(duplicates)
@@ -35,18 +39,19 @@ print(table)
 # print(board_nan.fen())
 # display.start(board_nan.fen())
 
-# os.makedirs("evaluation/data/", exist_ok = True)
+os.makedirs("evaluation/data/", exist_ok = True)
 
 # while True:
 #     pass
 
 scores = table["score"].values.tolist()
-print(scores)
-print(np.nanmin(scores))
-print(np.nanmax(scores))
 
 plt.figure()
 plt.hist(scores, bins = 50)
+plt.xlabel("Score")
+plt.ylabel("Number boards")
+plt.tight_layout()
+plt.savefig("evaluation/data/score_distribution.pdf")
 # plt.show()
 
 # argmin = table["score"].idxmin()
