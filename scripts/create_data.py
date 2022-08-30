4811f5e4ea7efdfdc3f85e96ba32c3a36ebd4ad7
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-08-30 15:29:23 +0200

    change number of boards per run to 20000

--- scripts/create_data.py
+++ scripts/create_data.py
@@ -1,25 +1,26 @@
 import time
-from utilities import *
+from utilities import boards_random
 import pandas as pd
 import os
+import numpy as np
 
 # to be added:
 # set stockfish depth
 # set stockfish skill level
 # set stockfish time limit
-number_runs = 1
+number_runs = 35
 for run in range(number_runs):
+    # run = run + 36
+    print(f"Processing run {run}...")
     # create random chess boards in "chess" and integer format
     start = time.time()
-    boards_random_fen, boards_random_int, boards_random_score = boards_random(num_boards = 10)
+    boards_random_fen, boards_random_int, boards_random_score = boards_random(num_boards = 20000)
 
     df1 = pd.DataFrame({"boards (FEN)": boards_random_fen})
     df2 = pd.DataFrame({"boards (int)": boards_random_int})
     df3 = pd.DataFrame({"score": boards_random_score})
 
     table = pd.concat([df1, df2, df3], axis = 1)
-
-    print(table)
 
     os.makedirs("data/", exist_ok = True)
 
