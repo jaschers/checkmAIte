8e0d6c91ba8103c5919da7951eead04997569eac
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-08-30 15:33:26 +0200

    Norm data label (score) between -1 and 1

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -7,37 +7,42 @@ from keras.callbacks import CSVLogger
 import pandas as pd
 import os
 import time
+from keras.applications import ResNet50
 
 # load data
-num_runs = 6
+num_runs = 35
 table = pd.DataFrame()
 for run in range(num_runs):
     print(f"Loading data run {run}...")
     start = time.time()
     table_run = pd.read_hdf(f"data/data{run}.h5", key = "table")
     middle = time.time()
-    print(f"Data run {run} loaded in {np.round(middle-start)} sec...")
     frame = [table, table_run]
     table = pd.concat(frame)
     end = time.time()
-    print(f"Tables combined in {np.round(end-middle)} sec...")
-table = table.dropna().reset_index()
+    print(f"Data run {run} loaded within {np.round(middle-start)} sec...")
+
+table = table.reset_index(drop = True)
+print(table)
 
 X = table["boards (int)"].values.tolist()
 Y = table["score"].values.tolist()
 
 # prepare data for neural network
 X_shape = np.shape(X)
-Y_shape = np.shape(Y)
 X = np.reshape(X, (X_shape[0], X_shape[1], X_shape[2], 1))
-# Y = np.reshape(Y, (Y_shape[0], 1))
 X_shape = np.shape(X)
-Y_shape = np.shape(Y)
+
+# norm Y data between -1 and 1
+Y_max = np.min(Y)
+Y = Y / Y_max
+
 X_train, X_val, X_test = np.split(X, [-int(len(X) / 5), -int(len(X) / 10)]) 
 Y_train, Y_val, Y_test = np.split(Y, [-int(len(X) / 5), -int(len(Y) / 10)]) 
 
-print(np.shape(X_train), np.shape(X_val), np.shape(X_test))
-print(np.shape(Y_train), np.shape(Y_val), np.shape(Y_test))
+# print(np.shape(X_train), np.shape(X_val), np.shape(X_test))
+# print(np.shape(Y_train), np.shape(Y_val), np.shape(Y_test))
+print("Number of training, validation and test data:", len(X_train), len(X_val), len(X_test))
 
 # define model
 model_input = Input(shape = X_shape[1:])
@@ -46,6 +51,11 @@ model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64], incr
 model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64])
 model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128], increase_dim = True)
 model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128])
+# model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [128, 256], increase_dim = True)
+# model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [128, 256])
+# model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [256, 512], increase_dim = True)
+# model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [256, 512])
+
 model = GlobalMaxPooling2D()(model)
 model = Flatten()(model)
 model = Dense(64, activation = "relu")(model)
@@ -64,17 +74,30 @@ model = Dense(1, name = "score")(model)
 # # model = Dense(16, activation = "relu")(model)
 # model = Dense(1, name = "score")(model)
 
+# model_input = Input(shape = X_shape[1:])
+# model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_input)
+# model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model)
+# model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model)
+# model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model)
+# model = Flatten()(model)
+# model = Dense(64, activation = "relu")(model)
+# model = Dense(1, name = "score")(model)
+
+# model_input = Input(shape = X_shape[1:])
+# model = Conv2D(3, (3, 3), activation = "relu", padding = "same")(model_input)
+# model = ResNet50()(model)
+
 model = models.Model(inputs = model_input, outputs = model)
 model.summary()
 
 os.makedirs("model/", exist_ok = True)
-utils.plot_model(model, to_file = "model/model.png", show_shapes = True, show_layer_names = False)
+# utils.plot_model(model, to_file = "model/model.pdf", show_shapes = True, show_layer_names = False)
 
 # compile model
 model.compile(optimizer = "adam",
               loss="mse")
 
 os.makedirs("history/", exist_ok = True)
-model.fit(X_train, Y_train, epochs = 20, batch_size = 64, validation_data=(X_val, Y_val), callbacks = [CSVLogger("history/history.csv")])
+model.fit(X_train, Y_train, epochs = 15, batch_size = 32, validation_data=(X_val, Y_val), callbacks = [CSVLogger("history/history.csv")])
 
 model.save("model/model.h5")
