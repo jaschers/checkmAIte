afa6b6b7421f1d3848ea69bdbc4f64e008e51268
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-08-30 15:32:00 +0200

    evaluate training and validation data

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -3,11 +3,11 @@ from keras import models
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from matplotlib.colors import ListedColormap
+from matplotlib.colors import ListedColormap, LogNorm
 import os
 
 # load data
-num_runs = 6
+num_runs = 35
 table = pd.DataFrame()
 for run in range(num_runs):
     print(f"Loading data run {run}...")
@@ -21,11 +21,11 @@ Y = table["score"].values.tolist()
 
 # prepare data for neural network
 X_shape = np.shape(X)
-Y_shape = np.shape(Y)
 X = np.reshape(X, (X_shape[0], X_shape[1], X_shape[2], 1))
-# Y = np.reshape(Y, (Y_shape[0], 1))
-X_shape = np.shape(X)
-Y_shape = np.shape(Y)
+
+Y_max = np.min(Y)
+Y = Y / Y_max
+
 X_train, X_val, X_test = np.split(X, [-int(len(X) / 5), -int(len(X) / 10)]) 
 Y_train, Y_val, Y_test = np.split(Y, [-int(len(X) / 5), -int(len(Y) / 10)]) 
 
@@ -45,8 +45,14 @@ plt.tight_layout()
 plt.savefig("evaluation/history.pdf")
 plt.close()
 
-prediction = model.predict(X_val)
-prediction = np.reshape(prediction, (np.shape(prediction)[0]))
+prediction_train = model.predict(X_train)
+prediction_train = np.reshape(prediction_train, (np.shape(prediction_train)[0]))
+
+prediction_val = model.predict(X_val)
+prediction_val = np.reshape(prediction_val, (np.shape(prediction_val)[0]))
+
+print(prediction_train)
+print(prediction_val)
 
 viridis = cm.get_cmap('viridis', 256)
 newcolors = viridis(np.linspace(0, 1, 256))
@@ -55,7 +61,7 @@ newcolors[:1, :] = white
 newcmp = ListedColormap(newcolors)
 
 plt.figure()
-plt.hist2d(Y_val, prediction, bins = (50, 50), cmap = newcmp)
+plt.hist2d(Y_val, prediction_val, bins = (50, 50), cmap = newcmp, norm = LogNorm())
 cbar = plt.colorbar()
 cbar.set_label('Number of boards')
 plt.plot(np.linspace(np.min(Y_val), np.max(Y_val), 100), np.linspace(np.min(Y_val), np.max(Y_val), 100), color = "black")
@@ -63,7 +69,20 @@ plt.ylim(np.min(Y_val), np.max(Y_val))
 plt.xlabel("True score")
 plt.ylabel("Predicted score")
 plt.tight_layout()
-plt.savefig("evaluation/2Dscattering.pdf")
+plt.savefig("evaluation/2Dscattering_val.pdf")
+# plt.show()
+plt.close()
+
+plt.figure()
+plt.hist2d(Y_train, prediction_train, bins = (50, 50), cmap = newcmp, norm = LogNorm())
+cbar = plt.colorbar()
+cbar.set_label('Number of boards')
+plt.plot(np.linspace(np.min(Y_train), np.max(Y_train), 100), np.linspace(np.min(Y_train), np.max(Y_train), 100), color = "black")
+plt.ylim(np.min(Y_train), np.max(Y_train))
+plt.xlabel("True score")
+plt.ylabel("Predicted score")
+plt.tight_layout()
+plt.savefig("evaluation/2Dscattering_train.pdf")
 # plt.show()
 plt.close()
 
