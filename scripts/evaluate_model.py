de5d5e224ac95bdaff70a9ac019467a7a5bab658
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-08-31 12:03:09 +0200

    read model predictions from file

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -1,5 +1,4 @@
 import numpy as np
-from keras import models
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -7,34 +6,22 @@ from matplotlib.colors import ListedColormap, LogNorm
 import os
 
 # load data
-num_runs = 35
-table = pd.DataFrame()
-for run in range(num_runs):
-    print(f"Loading data run {run}...")
-    table_run = pd.read_hdf(f"data/data{run}.h5", key = "table")
-    frame = [table, table_run]
-    table = pd.concat(frame)
-table = table.dropna().reset_index()
+print("Loading training data...")
+table_train = pd.read_hdf("prediction/prediction_train.h5", key = "table")
+print("Loading validation data...")
+table_val = pd.read_hdf("prediction/prediction_val.h5", key = "table")
 
-X = table["boards (int)"].values.tolist()
-Y = table["score"].values.tolist()
+prediction_train = table_train["prediction"] * 15000
+true_score_train = table_train["true score"] * 15000
 
-# prepare data for neural network
-X_shape = np.shape(X)
-X = np.reshape(X, (X_shape[0], X_shape[1], X_shape[2], 1))
-
-Y_max = np.min(Y)
-Y = Y / Y_max
-
-X_train, X_val, X_test = np.split(X, [-int(len(X) / 5), -int(len(X) / 10)]) 
-Y_train, Y_val, Y_test = np.split(Y, [-int(len(X) / 5), -int(len(Y) / 10)]) 
-
-model = models.load_model("model/model.h5")
+prediction_val = table_val["prediction"] * 15000
+true_score_val = table_val["true score"] * 15000
 
 history = pd.read_csv("history/history.csv")
 
 os.makedirs("evaluation/", exist_ok = True)
 
+print("Plotting history...")
 plt.figure()
 plt.plot(history["loss"], label="Training")
 plt.plot(history["val_loss"], label = "Validation")
@@ -45,27 +32,19 @@ plt.tight_layout()
 plt.savefig("evaluation/history.pdf")
 plt.close()
 
-prediction_train = model.predict(X_train)
-prediction_train = np.reshape(prediction_train, (np.shape(prediction_train)[0]))
-
-prediction_val = model.predict(X_val)
-prediction_val = np.reshape(prediction_val, (np.shape(prediction_val)[0]))
-
-print(prediction_train)
-print(prediction_val)
-
 viridis = cm.get_cmap('viridis', 256)
 newcolors = viridis(np.linspace(0, 1, 256))
 white = np.array([1, 1, 1, 1])
 newcolors[:1, :] = white
 newcmp = ListedColormap(newcolors)
 
+print("Plotting 2D scattering...")
 plt.figure()
-plt.hist2d(Y_val, prediction_val, bins = (50, 50), cmap = newcmp, norm = LogNorm())
+plt.hist2d(true_score_val, prediction_val, bins = (50, 50), cmap = newcmp, norm = LogNorm())
 cbar = plt.colorbar()
 cbar.set_label('Number of boards')
-plt.plot(np.linspace(np.min(Y_val), np.max(Y_val), 100), np.linspace(np.min(Y_val), np.max(Y_val), 100), color = "black")
-plt.ylim(np.min(Y_val), np.max(Y_val))
+plt.plot(np.linspace(np.min(true_score_val), np.max(true_score_val), 100), np.linspace(np.min(true_score_val), np.max(true_score_val), 100), color = "black")
+plt.ylim(np.min(true_score_val), np.max(true_score_val))
 plt.xlabel("True score")
 plt.ylabel("Predicted score")
 plt.tight_layout()
@@ -74,11 +53,11 @@ plt.savefig("evaluation/2Dscattering_val.pdf")
 plt.close()
 
 plt.figure()
-plt.hist2d(Y_train, prediction_train, bins = (50, 50), cmap = newcmp, norm = LogNorm())
+plt.hist2d(true_score_train, prediction_train, bins = (50, 50), cmap = newcmp, norm = LogNorm())
 cbar = plt.colorbar()
 cbar.set_label('Number of boards')
-plt.plot(np.linspace(np.min(Y_train), np.max(Y_train), 100), np.linspace(np.min(Y_train), np.max(Y_train), 100), color = "black")
-plt.ylim(np.min(Y_train), np.max(Y_train))
+plt.plot(np.linspace(np.min(true_score_train), np.max(true_score_train), 100), np.linspace(np.min(true_score_train), np.max(true_score_train), 100), color = "black")
+plt.ylim(np.min(true_score_train), np.max(true_score_train))
 plt.xlabel("True score")
 plt.ylabel("Predicted score")
 plt.tight_layout()
