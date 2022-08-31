98ee8cefebd3f5b3e03a024540b68ade61e803f6
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-08-31 12:03:27 +0200

    remove ResNet function

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -4,7 +4,6 @@ import numpy as np
 from stockfish import Stockfish
 import random
 from tqdm import tqdm
-from keras.layers import Conv2D, ReLU, Add, BatchNormalization
 import os 
 
 stockfish_path = os.environ.get("STOCKFISHPATH")
@@ -96,33 +95,3 @@ def boards_random(num_boards):
         boards_random_score.append(board_score(board.copy()))
 
     return(boards_random_fen, boards_random_int, boards_random_score)
-
-def ResBlock(z, kernelsizes, filters, increase_dim = False):
-    # https://github.com/priya-dwivedi/Deep-Learning/blob/master/resnet_keras/Residual_Networks_yourself.ipynb
-    # https://stackoverflow.com/questions/64792460/how-to-code-a-residual-block-using-two-layers-of-a-basic-cnn-algorithm-built-wit
-    # https://towardsdatascience.com/understanding-and-coding-a-resnet-in-keras-446d7ff84d33
-
-    z_shortcut = z
-    kernelsize_1, kernelsize_2 = kernelsizes
-    filters_1, filters_2 = filters
-
-    fz = Conv2D(filters_1, kernelsize_1)(z)
-    # fz = BatchNormalization()(fz)
-    fz = ReLU()(fz)
-
-    fz = Conv2D(filters_1, kernelsize_2, padding = "same")(fz)
-    # fz = BatchNormalization()(fz)
-    fz = ReLU()(fz)
-    
-    fz = Conv2D(filters_2, kernelsize_1)(fz)
-    # fz = BatchNormalization()(fz)
-
-    if increase_dim == True:
-        z_shortcut = Conv2D(filters_2, (1, 1))(z_shortcut)
-        # z_shortcut = BatchNormalization()(z_shortcut)
-
-    out = Add()([fz, z_shortcut])
-    out = ReLU()(out)
-    # out = MaxPooling2D(pool_size=(3, 3), strides = 1)(out)
-    
-    return out
