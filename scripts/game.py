08ac22be01042fb054d81d05c92c52cdbfe1feee
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-08-31 12:05:00 +0200

    add STOCKFISHPATH, print valid AI moves

--- scripts/game.py
+++ scripts/game.py
@@ -6,8 +6,10 @@ import chess.svg
 from chessboard import display
 import readline # allows to use arrow keys while being asked for user input
 
+stockfish_path = os.environ.get("STOCKFISHPATH")
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-engine = chess.engine.SimpleEngine.popen_uci("/usr/local/Cellar/stockfish/15/bin/stockfish")
+engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
 
 # load neural network model
 model = models.load_model("model/model.h5")
@@ -28,12 +30,14 @@ while True:
 
     # AI move
     valid_moves = list(board.legal_moves)
+    valid_moves_str = [valid_moves[i].uci() for i in range(len(valid_moves))]
+    print("Valid moves for AI:\n", valid_moves_str)
     valid_boards = []
     for i in range(len(valid_moves)):
         board.push(valid_moves[i])
         valid_boards.append(board_int(board.copy()))
         board.pop()
-    prediction = model.predict(valid_boards, verbose=0)
+    prediction = model.predict(valid_boards, verbose=0) * -15000
     print("AI all predicted scores: ", prediction)
     argmax = np.argmax(prediction)
     best_move = valid_moves[argmax]
@@ -51,12 +55,12 @@ while True:
     valid_moves = list(board.legal_moves)
     valid_moves_str = [valid_moves[i].uci() for i in range(len(valid_moves))]
     valid_moves_str.append("undo")
-    print("Valid moves:\n", valid_moves_str)
+    print("Valid moves for user:\n", valid_moves_str)
     user_input = input("Enter your move: ")
 
     while not (user_input in valid_moves_str):
         print("Invalid move!")
-        print("Valid moves:\n", valid_moves_str)
+        print("Valid moves for user:\n", valid_moves_str)
         user_input = input("Enter your move: ")
 
     if user_input == "undo":
