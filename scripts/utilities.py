7cb0b4f721ac4c0d330866c1e08571241ba93ddb
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-09-22 17:15:17 +0200

    add board with attacks

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -13,6 +13,13 @@ stockfish = Stockfish(stockfish_path)
 stockfish.set_depth(20)
 stockfish.set_skill_level(20)
 
+def square_to_index(square):
+    squares = np.linspace(0, 8*8 - 1, 8*8, dtype = int)
+    squares_2d = np.reshape(squares, (8,8))
+    squares_2d = np.flip(squares_2d, 0)
+    index = np.where(squares_2d == square)
+    return(index[0][0], index[1][0])
+
 def board_int(board):
     """converts chess board into 2D array with
     1: pawn
@@ -47,6 +54,68 @@ def board_int(board):
             board_index = np.unravel_index(square, (8, 8))
             # fill array at board_index with negative piece value 
             board_arr[board_index[0]][board_index[1]] = -piece
+    # board_arr = board_arr.flatten()
+    board_arr = board_arr.tolist()
+    return(board_arr)
+
+def board_3d_attack_int(board):
+    """converts chess board into 2D array with
+    1: pawn
+    2: knight
+    3: bishop
+    4: rook
+    5: queen
+    6: king
+    positive numbers: white
+    negative numbers: black
+
+    Args:
+        board (chess.Board): chess board in FEN format
+
+    Returns:
+        list: (8, 8) list of the input board with int values
+    """
+    # initialise board array
+    board_arr = np.zeros((13, 8, 8), dtype = int)
+
+    # for loop over all piece types (pawn, knight, ...)
+    for piece in chess.PIECE_TYPES:
+        # for loop over all squares of white pieces
+        for square in board.pieces(piece, chess.WHITE):
+            # get indices of the individual piece
+            board_index = np.unravel_index(square, (8, 8))
+            # fill array at board_index with piece value 
+            board_arr[0][board_index[0]][board_index[1]] = piece
+        # for loop over all squares of black pieces
+        for square in board.pieces(piece, chess.BLACK):
+            # get indices of the individual piece
+            board_index = np.unravel_index(square, (8, 8))
+            # fill array at board_index with negative piece value 
+            board_arr[0][board_index[0]][board_index[1]] = -piece
+
+    # add attacks from each pice to an individual 8x8 subarray
+    board.turn = chess.WHITE
+    for valid_move in board.legal_moves:
+        # get piece type that's making the move
+        piece_type = board.piece_type_at(valid_move.from_square)
+        # get square number that is being attacked
+        square = valid_move.to_square
+        # convert square number into index for (8,8) board
+        board_index = square_to_index(square)
+        # add +1 to the attacked square in the board_arr of corresponding piece type
+        board_arr[piece_type][board_index[0]][board_index[1]] += 1
+
+    board.turn = chess.BLACK
+    for valid_move in board.legal_moves:
+        # get piece type that's making the move
+        piece_type = board.piece_type_at(valid_move.from_square)
+        # get square number that is being attacked
+        square = valid_move.to_square
+        # convert square number into index for (8,8) board
+        board_index = square_to_index(square)
+        # add +1 to the attacked square in the board_arr of corresponding piece type
+        board_arr[piece_type + 6][board_index[0]][board_index[1]] += 1
+
     # board_arr = board_arr.flatten()
     board_arr = board_arr.tolist()
     return(board_arr)
