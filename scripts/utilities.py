04938f75962c05f1ada5c4682fd4e0b1063dc4c0
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-09-22 17:37:42 +0200

    add 3d board with 24 layers

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -59,39 +59,58 @@ def board_int(board):
     return(board_arr)
 
 def board_3d_attack_int(board):
-    """converts chess board into 2D array with
-    1: pawn
-    2: knight
-    3: bishop
-    4: rook
-    5: queen
-    6: king
-    positive numbers: white
-    negative numbers: black
+    """converts chess board into 3D (24, 8, 8) array with board[i] representing:
+    0: all squares covered by white pawn
+    1: all squares covered by white knight
+    2: all squares covered by white bishop
+    3: all squares covered by white rook
+    4: all squares covered by white queen
+    5: all squares covered by white king
+    6: all squares covered by black pawn
+    7: all squares covered by black knight
+    8: all squares covered by black bishop
+    9: all squares covered by black rook
+    10: all squares covered by black queen
+    11: all squares covered by black king
+    12: all squares being attacked by white pawn
+    13: all squares being attacked by white knight
+    14: all squares being attacked by white bishop
+    15: all squares being attacked by white rook
+    16: all squares being attacked by white queen
+    17: all squares being attacked by white king
+    18: all squares being attacked by black pawn
+    19: all squares being attacked by black knight
+    20: all squares being attacked by black bishop
+    21: all squares being attacked by black rook
+    22: all squares being attacked by black queen
+    23: all squares being attacked by black king
 
     Args:
-        board (chess.Board): chess board in FEN format
+        board (chess.Board): chess board
 
     Returns:
-        list: (8, 8) list of the input board with int values
+        list: (24, 8, 8) list of the input board with {1,0} int values
     """
     # initialise board array
-    board_arr = np.zeros((13, 8, 8), dtype = int)
+    number_boards = 24 
+    board_arr = np.zeros((number_boards, 8, 8), dtype = int)
 
     # for loop over all piece types (pawn, knight, ...)
     for piece in chess.PIECE_TYPES:
         # for loop over all squares of white pieces
         for square in board.pieces(piece, chess.WHITE):
             # get indices of the individual piece
-            board_index = np.unravel_index(square, (8, 8))
-            # fill array at board_index with piece value 
-            board_arr[0][board_index[0]][board_index[1]] = piece
+            board_index = square_to_index(square)
+            # fill array at board_index with piece value for each piece
+            board_arr[piece - 1][board_index[0]][board_index[1]] = 1
+
         # for loop over all squares of black pieces
         for square in board.pieces(piece, chess.BLACK):
             # get indices of the individual piece
-            board_index = np.unravel_index(square, (8, 8))
-            # fill array at board_index with negative piece value 
-            board_arr[0][board_index[0]][board_index[1]] = -piece
+            board_index = square_to_index(square)
+            # fill array at board_index with piece value for each piece
+            board_arr[piece - 1 + 6][board_index[0]][board_index[1]] = 1
+
 
     # add attacks from each pice to an individual 8x8 subarray
     board.turn = chess.WHITE
@@ -103,7 +122,7 @@ def board_3d_attack_int(board):
         # convert square number into index for (8,8) board
         board_index = square_to_index(square)
         # add +1 to the attacked square in the board_arr of corresponding piece type
-        board_arr[piece_type][board_index[0]][board_index[1]] += 1
+        board_arr[piece_type - 1 + 12][board_index[0]][board_index[1]] += 1
 
     board.turn = chess.BLACK
     for valid_move in board.legal_moves:
@@ -114,7 +133,7 @@ def board_3d_attack_int(board):
         # convert square number into index for (8,8) board
         board_index = square_to_index(square)
         # add +1 to the attacked square in the board_arr of corresponding piece type
-        board_arr[piece_type + 6][board_index[0]][board_index[1]] += 1
+        board_arr[piece_type - 1 + 18][board_index[0]][board_index[1]] += 1
 
     # board_arr = board_arr.flatten()
     board_arr = board_arr.tolist()
