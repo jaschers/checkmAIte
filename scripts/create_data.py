bfe67b1df59e386d394e90ebd27232e05e37de0a
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-10-01 10:00:43 +0200

    add 3dboards with player_move and halfmove_clock

--- scripts/create_data.py
+++ scripts/create_data.py
@@ -11,25 +11,26 @@ np.set_printoptions(threshold=sys.maxsize)
 # set stockfish depth
 # set stockfish skill level
 # set stockfish time limit
-number_runs = 10 # 60
+number_runs = 20 # 60
 for run in range(number_runs):
     run = run + 0
     print(f"Processing run {run}...")
     # create random chess boards in "chess" and integer format
-    start = time.time()
-    boards_random_int, boards_random_score = boards_random(num_boards = 10000) #10000
+    boards_random_int, player_move, halfmove_clock, fullmove_number, boards_random_score = boards_random(num_boards = 5000) #10000
 
-    df1 = pd.DataFrame({"board3d (int)": boards_random_int})
-    df2 = pd.DataFrame({"score": boards_random_score})
+    df1 = pd.DataFrame({"board3d": boards_random_int})
+    df2 = pd.DataFrame({"player move": player_move})
+    df3 = pd.DataFrame({"halfmove clock": halfmove_clock})
+    df4 = pd.DataFrame({"fullmove number": fullmove_number})
+    df5 = pd.DataFrame({"score": boards_random_score})
 
-    table = pd.concat([df1, df2], axis = 1)
+    table = pd.concat([df1, df2, df3, df4, df5], axis = 1)
 
     print(table)
     # print(np.array(table["board3d (int)"][0]))
 
-    os.makedirs("data/3d/digital_secrets/", exist_ok = True)
+    os.makedirs("data/3d/24_8_8_depth20/", exist_ok = True)
 
-    table.to_hdf(f"data/3d/digital_secrets/data{run}.h5", key = "table")
+    table.to_hdf(f"data/3d/24_8_8_depth20/data{run}.h5", key = "table")
 
     end = time.time()
-    print(f"Processing time for run {run}:", np.round((end - start) / 60 / 60, 2), "hours")
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,7 +1,7 @@
 import numpy as np
 from utilities_keras import *
 from keras import models, optimizers
-from keras.layers import Conv2D, GlobalMaxPooling2D, Dense, Flatten, Input, BatchNormalization, Add, Activation
+from keras.layers import Conv2D, GlobalMaxPooling2D, Dense, Flatten, Input, BatchNormalization, Add, Activation, Concatenate
 import keras.utils as utils
 from keras.callbacks import CSVLogger, ReduceLROnPlateau, EarlyStopping
 import pandas as pd
@@ -9,6 +9,8 @@ import os
 import time
 import argparse
 import matplotlib.pyplot as plt
+import sys
+np.set_printoptions(threshold=sys.maxsize)
 
 ######################################## argparse setup ########################################
 script_descr="""
@@ -27,12 +29,12 @@ args = parser.parse_args()
 
 
 # load data
-num_runs = 10
+num_runs = 19 #19
 table = pd.DataFrame()
 for run in range(num_runs):
     print(f"Loading data run {run}...")
     start = time.time()
-    table_run = pd.read_hdf(f"data/3d/data{run}.h5", key = "table")
+    table_run = pd.read_hdf(f"data/3d/24_8_8_depth15/data{run}.h5", key = "table")
     middle = time.time()
     frame = [table, table_run]
     table = pd.concat(frame)
@@ -40,63 +42,63 @@ for run in range(num_runs):
     print(f"Data run {run} loaded within {np.round(middle-start, 1)} sec...")
 
 table = table.reset_index(drop = True)
+table = table.drop(table[abs(table.score) > 10900].index)
+table = table.reset_index(drop = True)
 print(table)
 
-X = table["board3d (int)"].values.tolist()
+X_board3d = table["board3d"].values.tolist()
+X_parameter = table[["player move", "halfmove clock"]].values.tolist()
 Y = table["score"].values.tolist()
 
 # prepare data for neural network
-# X_shape = np.shape(X)
-X = np.moveaxis(X, 1, -1)
-# X = np.reshape(X, (X_shape[0], 8, 8, 14))
-X_shape = np.shape(X)
+# X_board3d = np.zeros((len(X_board3d_aux), 14, 8, 8), dtype = int)
+# X_board3d[:,:12] = np.array(X_board3d_aux)[:,:12]
+# X_board3d[:,12] = np.sum(np.array(X_board3d_aux)[0,12:18], axis = 0)
+# X_board3d[:,13] = np.sum(np.array(X_board3d_aux)[0,18:], axis = 0)
+
+X_board3d = np.moveaxis(X_board3d, 1, -1)
+X_board3d_shape = np.shape(X_board3d)
+X_parameter_shape = np.shape(X_parameter)
+
+print("X board3d shape:", np.shape(X_board3d))
+print("X parameter shape:", np.shape(X_parameter))
+print("Y score shape:", np.shape(Y))
 
 # norm Y data between -1 and 1
-print(np.min(Y), np.max(Y))
-# Y_max = np.max(Y)
-# Y = Y / Y_max
-# Y = np.asarray(Y / abs(np.asarray(Y)).max() / 2 + 0.5, dtype=np.float32)
+print("Ymin, Ymax before normalisation:", np.min(Y), np.max(Y))
 Y = Y - np.min(Y)
 Y = Y / np.max(Y)
-print(np.min(Y), np.max(Y))
+# Y = np.asarray(Y / abs(np.array(Y)).max() / 2 + 0.5, dtype=np.float32) # normalization (0 - 1)
+print("Ymin, Ymax after normalisation:", np.min(Y), np.max(Y))
 
+X_board3d_train, X_board3d_val, X_board3d_test = np.split(X_board3d, [-int(len(X_board3d) / 5), -int(len(X_board3d) / 10)]) 
+X_parameter_train, X_parameter_val, X_parameter_test = np.split(X_parameter, [-int(len(X_parameter) / 5), -int(len(X_parameter) / 10)]) 
+Y_train, Y_val, Y_test = np.split(Y, [-int(len(Y) / 5), -int(len(Y) / 10)]) 
 
-X_train, X_val, X_test = np.split(X, [-int(len(X) / 5), -int(len(X) / 10)]) 
-Y_train, Y_val, Y_test = np.split(Y, [-int(len(X) / 5), -int(len(Y) / 10)]) 
-
-# print(np.shape(X_train), np.shape(X_val), np.shape(X_test))
+# print(np.shape([X_board3d, X_parameter]), np.shape(X_val), np.shape(X_test))
 # print(np.shape(Y_train), np.shape(Y_val), np.shape(Y_test))
-print("Number of training, validation and test data:", len(X_train), len(X_val), len(X_test))
+print("Number of training, validation and test data:", len(X_board3d_train), len(X_board3d_val), len(X_board3d_test))
 
-# define model
-model_input = Input(shape = X_shape[1:])
-model = Conv2D(16, kernel_size = (3, 3), activation = "relu", padding = "same")(model_input)
-model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64], increase_dim = True)
-model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64])
-model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128], increase_dim = True)
-model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128])
-# model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [128, 256], increase_dim = True)
-# model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [128, 256])
-# model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [256, 512], increase_dim = True)
-# model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [256, 512])
-# model = ResBlock(model,)GlobalMaxPooling2D()(model)
-model = Flatten()(model)
-model = Dense(128, activation = "relu")(model)
-model = Dense(1, name = "score")(model)
+# # # define model
+# ############################################################################
+# # model_input = Input(shape = X_shape[1:])
+# # model = Conv2D(16, kernel_size = (3, 3), activation = "relu", padding = "same")(model_input)
+# # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64], increase_dim = True)
+# # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64])
+# # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128], increase_dim = True)
+# # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128])
+# # # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [128, 256], increase_dim = True)
+# # # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [128, 256])
+# # # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [256, 512], increase_dim = True)
+# # # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [256, 512])
+# # # model = ResBlock(model,)GlobalMaxPooling2D()(model)
+# # model = Flatten()(model)
+# # model = Dense(128, activation = "relu")(model)
+# # model = Dense(1, name = "score")(model)
+# ############################################################################
 
-# # define model
-# model_input = Input(shape = X_shape[1:])
-# model = Conv2D(16, (3, 3), activation = "relu", padding = "same")(model_input)
-# model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model)
-# model = Conv2D(64, (3, 3), activation = "relu", padding = "same")(model)
-# model = Conv2D(128, (3, 3), activation = "relu", padding = "same")(model)
-# model = GlobalMaxPooling2D()(model)
-# model = Flatten()(model)
-# model = Dense(128, activation = "relu")(model)
-# model = Dense(64, activation = "relu")(model)
-# # model = Dense(16, activation = "relu")(model)
-# model = Dense(1, name = "score")(model)
 
+# ############################################################################
 # model_input = Input(shape = X_shape[1:])
 # model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_input)
 # model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model)
@@ -105,73 +107,66 @@ model = Dense(1, name = "score")(model)
 # model = Flatten()(model)
 # model = Dense(64, activation = "relu")(model)
 # model = Dense(1, name = "score")(model)
-
-# model_input = Input(shape = X_shape[1:])
-# model = Conv2D(3, (3, 3), activation = "relu", padding = "same")(model_input)
-# model = ResNet50()(model)
-
-# # digital secrets CNN model
-# model_input = Input(shape = X_shape[1:])
-# def build_model(conv_size, conv_depth):
-#   board3d = Input(shape=(8, 8, 24)) #shape=(14, 8, 8)
-
-#   # adding the convolutional layers
-#   x = board3d
-#   for _ in range(conv_depth):
-#     x = Conv2D(filters=conv_size, kernel_size=3, padding="same", activation="relu")(x) #data_format="channels_first"
-#   x = Flatten()(x)
-#   x = Dense(64, "relu")(x)
-#   x = Dense(1, "sigmoid")(x)
-
-#   return models.Model(inputs=board3d, outputs=x)
-
-# model = build_model(32, 4)
-
-# # digital secrets ResNet model
-# model_input = Input(shape = X_shape[1:])
-# model = Conv2D(filters=32, kernel_size=3, padding="same")(model_input)
-# for _ in range(4):
-#     previous = model
-#     model = Conv2D(filters=32, kernel_size=3, padding="same")(model)
-#     model = BatchNormalization()(model)
-#     model = Activation("relu")(model)
-#     model = Conv2D(filters=32, kernel_size=3, padding="same")(model)
-#     model = BatchNormalization()(model)
-#     model = Add()([model, previous])
-#     model = Activation("relu")(model)
-# model = Flatten()(model)
-# model = Dense(1)(model)
+# ############################################################################
 
 
-model = models.Model(inputs = model_input, outputs = model)
+############################################################################
+model_input_board3d = Input(shape = X_board3d_shape[1:])
+model_board3d = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_input_board3d)
+model_board3d = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_board3d)
+model_board3d = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_board3d)
+model_board3d = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_board3d)
+model_board3d = Flatten()(model_board3d)
+
+model_input_parameter = Input(shape = X_parameter_shape[1:])
+# model_parameter = Dense(128, activation = "relu")(model_input_parameter)
+
+model = Concatenate()([model_board3d, model_input_parameter])
+
+model = Dense(128, activation = "relu")(model)
+model = Dense(64, activation = "relu")(model)
+model = Dense(1, name = "score")(model)
+############################################################################
+
+# ############################################################################
+# model_input_board3d = Input(shape = X_board3d_shape[1:])
+# model_board3d = Conv2D(16, kernel_size = (3, 3), activation = "relu", padding = "same")(model_input_board3d)
+# model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64], increase_dim = True)
+# model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64])
+# model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128], increase_dim = True)
+# model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128])
+# model_board3d = Flatten()(model_board3d)
+
+# model_input_parameter = Input(shape = X_parameter_shape[1:])
+# # model_parameter = Dense(128, activation = "relu")(model_input_parameter)
+
+# model = Concatenate()([model_board3d, model_input_parameter])
+
+# model = Dense(128, activation = "relu")(model)
+# model = Dense(64, activation = "relu")(model)
+# model = Dense(1, name = "score")(model)
+# ############################################################################
+
+model = models.Model(inputs = [model_input_board3d, model_input_parameter], outputs = model)
 model.summary()
 
 os.makedirs("model/", exist_ok = True)
-# utils.plot_model(model, to_file = "model/model.pdf", show_shapes = True, show_layer_names = False)
-
-# # add a decaying learning rate
-# lr_schedule = optimizers.schedules.ExponentialDecay(initial_learning_rate=1e-2, decay_steps=10000, decay_rate=0.9)
-# optimizer = optimizers.SGD(learning_rate=lr_schedule)
-
-# # compile model
-# model.compile(optimizer = optimizer,
-#               loss="mse")
 
 # compile model
 model.compile(optimizer=optimizers.Adam(5e-4), loss="mse")
 
 os.makedirs("history/", exist_ok = True)
-model.fit(X_train, Y_train, epochs = args.epochs, batch_size = 2048, validation_data=(X_val, Y_val), callbacks = [CSVLogger(f"history/history_{args.name}.csv"), ReduceLROnPlateau(monitor="val_loss", patience=10), EarlyStopping(monitor="val_loss", patience=15, min_delta=1e-4)])
+model.fit([X_board3d_train, X_parameter_train], Y_train, epochs = args.epochs, batch_size = 32, validation_data=([X_board3d_val, X_parameter_val], Y_val), callbacks = [CSVLogger(f"history/history_{args.name}.csv"), ReduceLROnPlateau(monitor="val_loss", patience=10), EarlyStopping(monitor="val_loss", patience=15, min_delta=1e-4)])
 
 model.save(f"model/model_{args.name}.h5")
 
 # save model predictions on training an validation data
 os.makedirs("prediction/", exist_ok = True)
 
-prediction_train = model.predict(X_train)
+prediction_train = model.predict([X_board3d_train, X_parameter_train])
 prediction_train = np.reshape(prediction_train, (np.shape(prediction_train)[0]))
 
-prediction_val = model.predict(X_val)
+prediction_val = model.predict([X_board3d_val, X_parameter_val])
 prediction_val = np.reshape(prediction_val, (np.shape(prediction_val)[0]))
 
 table_pred_train = pd.DataFrame({"prediction": prediction_train})
@@ -183,8 +178,8 @@ table_true_val = pd.DataFrame({"true score": Y_val})
 table_pred_train = pd.concat([table_pred_train, table_true_train], axis = 1)
 table_pred_val = pd.concat([table_pred_val, table_true_val], axis = 1)
 
-print(table_pred_train)
-print(table_pred_val)
+# print(table_pred_train)
+# print(table_pred_val)
 
 table_pred_train.to_hdf(f"prediction/prediction_train_{args.name}.h5", key = "table")
 table_pred_val.to_hdf(f"prediction/prediction_val_{args.name}.h5", key = "table")
