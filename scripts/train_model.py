2204d2ce5b2809cb7649a2c869971ea0e54a4e61
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-12-16 20:49:40 +0100

    add sc range, resnet512 and save best model

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,9 +1,11 @@
 import numpy as np
 from utilities_keras import *
+from tensorflow.keras.optimizers import Adam
 from keras import models, optimizers
-from keras.layers import Conv2D, GlobalMaxPooling2D, Dense, Flatten, Input, BatchNormalization, Add, Activation, Concatenate
+from keras.layers import Conv2D, GlobalMaxPooling2D, Dense, Flatten, Input, BatchNormalization, Add, Activation, Concatenate, Dropout
+# from keras.applications import EfficientNetV2L, ResNet152V2, VGG19, EfficientNetV2M, EfficientNetV2S, ResNet50
 import keras.utils as utils
-from keras.callbacks import CSVLogger, ReduceLROnPlateau, EarlyStopping
+from keras.callbacks import CSVLogger, ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
 import pandas as pd
 import os
 import time
@@ -22,13 +24,12 @@ parser = argparse.ArgumentParser(description=script_descr)
 # Define expected arguments
 parser.add_argument("-r", "--runs", type = int, required = True, metavar = "-", help = "Number of runs used for training")
 parser.add_argument("-nd", "--name_data", type = str, required = False, metavar = "-", default = "34_8_8_depth0_mm100_ms15000", help = "name of the data folder")
-parser.add_argument("-sc", "--score_cut", type = float, required = False, metavar = "-", default = None, help = "score cut applied on the data (default: None)")
+parser.add_argument("-sc", "--score_cut", type = float, required = False, nargs='+', metavar = "-", default = None, help = "score cut applied on the data (default: None)")
 parser.add_argument("-ne", "--name_experiment", type = str, required = True, metavar = "-", help = "Name of this particular experiment")
 parser.add_argument("-e", "--epochs", type = int, required = False, metavar = "-", default = 1000, help = "Number of epochs for the training")
 
 args = parser.parse_args()
 ##########################################################################################
-
 
 # load data
 X_board3d, X_parameter, Y = load_data(args.runs, args.name_data, args.score_cut)
@@ -43,10 +44,12 @@ print("Y score shape:", np.shape(Y))
 
 # norm Y data between -1 and 1
 print("Y_score_min, Y_score_max before normalisation:", np.min(Y[:,0]), np.max(Y[:,0]))
+print("Y_score unique", np.unique(Y[:,0]))
 Y = Y.astype("float")
 Y[:,0] = Y[:,0] - np.min(Y[:,0])
 Y[:,0] = Y[:,0] / np.max(Y[:,0])
 print("Y_score_min, Y_score_max after normalisation:", np.min(Y[:,0]), np.max(Y[:,0]))
+# Y = Y[:,0]
 
 X_board3d_train, X_board3d_val, X_board3d_test = np.split(X_board3d, [-int(len(X_board3d) / 5), -int(len(X_board3d) / 10)]) 
 X_parameter_train, X_parameter_val, X_parameter_test = np.split(X_parameter, [-int(len(X_parameter) / 5), -int(len(X_parameter) / 10)]) 
@@ -58,41 +61,12 @@ print("Number of training, validation and test data:", len(X_board3d_train), len
 
 # # # define model
 # ############################################################################
-# # model_input = Input(shape = X_shape[1:])
-# # model = Conv2D(16, kernel_size = (3, 3), activation = "relu", padding = "same")(model_input)
-# # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64], increase_dim = True)
-# # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64])
-# # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128], increase_dim = True)
-# # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128])
-# # # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [128, 256], increase_dim = True)
-# # # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [128, 256])
-# # # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [256, 512], increase_dim = True)
-# # # model = ResBlock(model, kernelsizes = [(1, 1), (3, 3)], filters = [256, 512])
-# # # model = ResBlock(model,)GlobalMaxPooling2D()(model)
-# # model = Flatten()(model)
-# # model = Dense(128, activation = "relu")(model)
-# # model = Dense(1, name = "score")(model)
-# ############################################################################
-
-
-# ############################################################################
-# model_input = Input(shape = X_shape[1:])
-# model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_input)
-# model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model)
-# model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model)
-# model = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model)
-# model = Flatten()(model)
-# model = Dense(64, activation = "relu")(model)
-# model = Dense(1, name = "score")(model)
-# ############################################################################
-
-
-# ############################################################################
 # model_input_board3d = Input(shape = X_board3d_shape[1:])
-# model_board3d = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_input_board3d)
-# model_board3d = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_board3d)
-# model_board3d = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_board3d)
-# model_board3d = Conv2D(32, (3, 3), activation = "relu", padding = "same")(model_board3d)
+# model_board3d = Conv2D(16, kernel_size = (3, 3), activation = "relu", padding = "same")(model_input_board3d)
+# model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64], increase_dim = True)
+# model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64])
+# model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128], increase_dim = True)
+# model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128])
 # model_board3d = Flatten()(model_board3d)
 
 # model_input_parameter = Input(shape = X_parameter_shape[1:])
@@ -102,16 +76,17 @@ print("Number of training, validation and test data:", len(X_board3d_train), len
 
 # model = Dense(128, activation = "relu")(model)
 # model = Dense(64, activation = "relu")(model)
-# model = Dense(1, name = "score")(model)
+# model = Dense(4, name = "score-check-checkmate-stalemate")(model)
+# # model = Dense(1, name = "score-check-checkmate-stalemate")(model)
 # ############################################################################
 
-############################################################################
+######################################################
 model_input_board3d = Input(shape = X_board3d_shape[1:])
-model_board3d = Conv2D(16, kernel_size = (3, 3), activation = "relu", padding = "same")(model_input_board3d)
-model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64], increase_dim = True)
-model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [32, 64])
-model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128], increase_dim = True)
-model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [64, 128])
+model_board3d = Conv2D(64, kernel_size = (3, 3), activation = "relu", padding = "same")(model_input_board3d)
+model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [64, 256], increase_dim = True)
+model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [64, 256])
+model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [256, 512], increase_dim = True)
+model_board3d = ResBlock(model_board3d, kernelsizes = [(1, 1), (3, 3)], filters = [256, 512])
 model_board3d = Flatten()(model_board3d)
 
 model_input_parameter = Input(shape = X_parameter_shape[1:])
@@ -119,10 +94,43 @@ model_input_parameter = Input(shape = X_parameter_shape[1:])
 
 model = Concatenate()([model_board3d, model_input_parameter])
 
+model = Dense(256, activation = "relu")(model)
+model = Dropout(0.05)(model)
 model = Dense(128, activation = "relu")(model)
-model = Dense(64, activation = "relu")(model)
+model = Dropout(0.05)(model)
 model = Dense(4, name = "score-check-checkmate-stalemate")(model)
+# model = Dense(1, name = "score-check-checkmate-stalemate")(model)
 ############################################################################
+
+# ############################################################################
+# model_input_board3d = Input(shape = X_board3d_shape[1:])
+
+# model_board3d = EfficientNetV2S(weights = None, include_top = True, classes = 1024, classifier_activation = None, input_tensor = model_input_board3d) #, input_shape = (8, 8, 34))
+
+# model_input_parameter = Input(shape = X_parameter_shape[1:])
+# # model_parameter = Dense(128, activation = "relu")(model_input_parameter)
+
+# print("______________________")
+# print(model_board3d)
+# print(model_board3d.output)
+# print(model_input_parameter)
+# print("______________________")
+
+# model = Concatenate()([model_board3d.output, model_input_parameter])
+
+# model = Dense(512, activation = "relu")(model)
+# # model = Dense(64, activation = "relu")(model)
+# model = Dense(4, name = "score-check-checkmate-stalemate")(model)
+
+# model = models.Model(inputs = [model_input_board3d, model_input_parameter], outputs = model)
+
+# model.summary()
+
+# model.compile(optimizer = "adam", loss="mse") # mean_squared_logarithmic_error
+# checkpointer = ModelCheckpoint(filepath = f"model/model_{args.name_experiment}.h5", verbose = 1, save_best_only = True)
+# os.makedirs("history/", exist_ok = True)
+# model.fit([X_board3d_train, X_parameter_train], Y_train, epochs = args.epochs, batch_size = 32, validation_data=([X_board3d_val, X_parameter_val], Y_val), callbacks = [checkpointer, CSVLogger(f"history/history_{args.name_experiment}.csv"), ReduceLROnPlateau(monitor="val_loss", patience=30, min_delta=1e-5), EarlyStopping(monitor="val_loss", patience=50, min_delta=1e-5)], verbose = 2)
+# ############################################################################
 
 model = models.Model(inputs = [model_input_board3d, model_input_parameter], outputs = model)
 model.summary()
@@ -131,12 +139,13 @@ os.makedirs("model/", exist_ok = True)
 
 # compile model
 # model.compile(optimizer=optimizers.Adam(5e-4), loss="mse")
-model.compile(optimizer = "adam", loss="mse") # mean_squared_logarithmic_error
-
+# model.compile(optimizer = "adam", loss = "mse", learning_rate = 1e-4) # mean_squared_logarithmic_error
+model.compile(optimizer = Adam(learning_rate = 1e-4), loss="mse") # mean_squared_logarithmic_error
+checkpointer = ModelCheckpoint(filepath = f"model/model_{args.name_experiment}.h5", verbose = 1, save_best_only = True)
 os.makedirs("history/", exist_ok = True)
-model.fit([X_board3d_train, X_parameter_train], Y_train, epochs = args.epochs, batch_size = 32, validation_data=([X_board3d_val, X_parameter_val], Y_val), callbacks = [CSVLogger(f"history/history_{args.name_experiment}.csv"), ReduceLROnPlateau(monitor="val_loss", patience=30, min_delta=1e-5), EarlyStopping(monitor="val_loss", patience=50, min_delta=1e-5)], verbose = 2)
+model.fit([X_board3d_train, X_parameter_train], Y_train, epochs = args.epochs, batch_size = 32, validation_data = ([X_board3d_val, X_parameter_val], Y_val), callbacks = [checkpointer, CSVLogger(f"history/history_{args.name_experiment}.csv"), ReduceLROnPlateau(monitor = "val_loss", patience = 20, min_delta = 1e-7), EarlyStopping(monitor = "val_loss", patience = 40, min_delta = 1e-7)], verbose = 2)
 
-model.save(f"model/model_{args.name_experiment}.h5")
+# model.save(f"model/model_{args.name_experiment}.h5")
 
 # # save model predictions on training an validation data
 # os.makedirs("prediction/", exist_ok = True)
