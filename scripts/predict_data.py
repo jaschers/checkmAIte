e42177e159fcbd2326c799a662d97d4d6ee9fef8
Jann Aschersleben <Jann.Aschersleben@rwth-aachen.de>
2022-12-16 20:48:16 +0100

    add score cut range

--- scripts/predict_data.py
+++ scripts/predict_data.py
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser(description=script_descr)
 # Define expected arguments
 parser.add_argument("-r", "--runs", type = int, required = True, metavar = "-", help = "Number of runs used for training")
 parser.add_argument("-nd", "--name_data", type = str, required = False, metavar = "-", default = "34_8_8_depth0_mm100_ms15000", help = "name of the data folder")
-parser.add_argument("-sc", "--score_cut", type = float, required = False, metavar = "-", default = None, help = "score cut applied on the data (default: None)")
+parser.add_argument("-sc", "--score_cut", type = float, required = False, nargs='+', metavar = "-", default = None, help = "score cut applied on the data (default: None)")
 parser.add_argument("-ne", "--name_experiment", type = str, required = True, metavar = "-", help = "Name of this particular experiment")
 
 args = parser.parse_args()
@@ -40,6 +40,7 @@ print("Y score shape:", np.shape(Y))
 
 # norm Y data between -1 and 1
 print("Y_score_min, Y_score_max before normalisation:", np.min(Y[:,0]), np.max(Y[:,0]))
+print("Y_score unique", np.unique(Y[:,0]))
 Y = Y.astype("float")
 Y[:,0] = Y[:,0] - np.min(Y[:,0])
 Y[:,0] = Y[:,0] / np.max(Y[:,0])
@@ -66,11 +67,12 @@ df5 = pd.DataFrame({"predicted checkmate": prediction_val[:,2]})
 df6 = pd.DataFrame({"true checkmate": Y_val[:,2]})
 df7 = pd.DataFrame({"predicted stalemate": prediction_val[:,3]})
 df8 = pd.DataFrame({"true stalemate": Y_val[:,3]})
+df9 = pd.DataFrame({"player move": X_parameter_val[:,0]})
 
-table_pred_val = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8], axis = 1)
+table_pred_val = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9], axis = 1)
 
 print(table_pred_val)
 
 table_pred_val.to_hdf(f"prediction/{args.name_experiment}/prediction_val_{args.name_experiment}.h5", key = "table")
 
-# get_extreme_predictions(prediction_val, Y_val, X_board3d, X_parameter, args.name_experiment)
+get_extreme_predictions(table_pred_val["predicted score"], table_pred_val["true score"], X_board3d_val, X_parameter_val, args.name_experiment)
